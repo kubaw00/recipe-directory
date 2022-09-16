062156f6947441994d466d689cda76bbd87f6899
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-17 00:28:12 +0200

    add multiple ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Create.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Create() {
   const [title, setTitle] = useState('');
+  const [newIngredient, setNewIngredient] = useState('');
+  const [ingredients, setIngredients] = useState([]);
   const [method, setMethod] = useState('');
   const [cookingTime, setCookingTime] = useState('');
+  const ref = useRef();
 
   function onSubmit(e) {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+  }
+
+  function addIngridient() {
+    if (!ingredients.includes(newIngredient) && newIngredient !== null) {
+      setIngredients((prevState) => [...prevState, newIngredient.trim()]);
+    }
+    setNewIngredient('');
+    ref.current.focus();
   }
 
   return (
@@ -20,9 +30,27 @@ export default function Create() {
           <span>Recipe title</span>
           <input
             value={title}
+            ref={ref}
             onChange={(e) => setTitle(e.target.value)}
           ></input>
         </label>
+
+        <label>
+          <span>Recipe ingredients</span>
+          <div className='ingredients'>
+            <input
+              value={newIngredient}
+              onChange={(e) => setNewIngredient(e.target.value)}
+            ></input>
+            <button onClick={addIngridient}>add</button>
+          </div>
+        </label>
+        <p>
+          Current ingredients:{' '}
+          {ingredients.map((ing) => (
+            <em key={ing}>{`${ing}, `} </em>
+          ))}
+        </p>
         <label>
           <span>Recipe Metchod</span>
           <textarea
@@ -30,6 +58,7 @@ export default function Create() {
             onChange={(e) => setMethod(e.target.value)}
           ></textarea>
         </label>
+
         <label>
           <span>Cooking Time (minutes):</span>
           <input
