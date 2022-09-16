3c5a771e1fe32a9962cebdf8b112a54c52f3e724
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-16 03:20:25 +0200

    pages setup & Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css'
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/recipe/:id'>
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
