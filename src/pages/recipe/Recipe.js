1732a918a7c266d74333dec524e8ecb43b2a145d
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-16 22:26:07 +0200

    fetch single recipe useParams hook

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './Recipe.css';
+import { useFetch } from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  console.log(id);
+  const {
+    data: recipe,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:3000/recipes/${id}`);
+
+  return (
+    <div className='recipe'>
+      {error && <div className='error'>{error}</div>}
+      {isPending && <div className='laoding'> Loading ...</div>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
+  );
 }
