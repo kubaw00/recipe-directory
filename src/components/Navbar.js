644c6f7a01011b4c1ec35f55570dba99fc2c66ed
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-19 16:25:34 +0200

    create context and add context to navbar as background color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import { ThemeContext } from '../context/ThemeContext';
+import { useContext } from 'react';
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext);
+
   return (
-    <div className='navbar'>
+    <div style={{ backgroundColor: color }} className='navbar'>
       <nav>
         <Link className='brand' to='/'>
           <h1>Cooking</h1>
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+export default function ThemeProvider({ children }) {
+  return (
+    <ThemeContext.Provider value={{ color: 'blue' }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
