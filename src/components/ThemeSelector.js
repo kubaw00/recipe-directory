f3e7ee6e70156ed855632378e557afaa0f2241ef
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-19 18:37:30 +0200

    add mode state and themeSelector

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './ThemeSelector.css';
 import useTheme from '../hooks/useTheme';
+import changeModeIcon from '../assets/changeMode.svg';
 
 const themeColors = ['#58249c', '#249c6b', '#b70233'];
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark');
+  };
+  console.log(mode);
 
   return (
     <div className='theme-selector'>
+      <div className='mode-toggle'>
+        <img
+          src={changeModeIcon}
+          alt='change mode dark/light'
+          onClick={toggleMode}
+          style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(50%)' }}
+        ></img>
+      </div>
       <div className='theme-buttons'>
         {themeColors.map((color) => (
           <div
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -9,20 +9,29 @@ const themeReducer = (state, action) => {
   switch (action.type) {
     case 'CHANGE_COLOR':
       return { ...state, color: action.payload };
+    case 'CHANGE_MODE':
+      return { ...state, mode: action.payload };
     default:
       return state;
   }
 };
 
 export default function ThemeProvider({ children }) {
-  const [state, dispatch] = useReducer(themeReducer, { color: 'blue' });
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: 'blue',
+    mode: 'dark',
+  });
 
   const changeColor = (color) => {
     dispatch({ type: 'CHANGE_COLOR', payload: color });
   };
 
+  const changeMode = (mode) => {
+    dispatch({ type: 'CHANGE_MODE', payload: mode });
+  };
+
   return (
-    <ThemeContext.Provider value={{ ...state, changeColor }}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
       {children}
     </ThemeContext.Provider>
   );
